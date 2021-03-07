f4414f891ba97d3220342c7e55cec3637a94b1e4
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-07 11:37:18 +0000

    File formatted

--- frontend/src/barbers.js
+++ frontend/src/barbers.js
@@ -1,24 +1,21 @@
 const barbers = [
-     {
-          '_id':'1',
-          'name': 'John Mckenny',
-          'image': '/images/barbers/John.jpg',
-          'rating':3.5,
-          
-     },
-     {
-          '_id': '2',
-          'name': 'Peter FitzGerald',
-          'image': '/images/barbers/Peter.jpg',
-          'rating':4.5,
-
-     },
-     {
-          '_id': '3',
-          'name': 'Marie Kennedy',
-          'image': '/images/barbers/Marie.jpg',
-          'rating':3.7,
-
-     }
-]
-export default barbers
+  {
+    _id: "1",
+    name: "John Mckenny",
+    image: "/images/barbers/John.jpg",
+    rating: 3.5,
+  },
+  {
+    _id: "2",
+    name: "Peter FitzGerald",
+    image: "/images/barbers/Peter.jpg",
+    rating: 4.5,
+  },
+  {
+    _id: "3",
+    name: "Marie Kennedy",
+    image: "/images/barbers/Marie.jpg",
+    rating: 3.7,
+  },
+];
+export default barbers;
