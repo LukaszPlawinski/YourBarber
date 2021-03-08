29c4f35a8512db46ad44bea9548eac548e054191
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-08 19:13:35 +0000

    implemented axios fetch

--- frontend/src/pages/Barbers.js
+++ frontend/src/pages/Barbers.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
-import barbers from "../barbers";
 import Barber from "../components/Barber";
 
 function Barbers() {
+  const [barbers, setBarbers] = useState([]);
+
+  useEffect(() => {
+    async function fetchBarbers() {
+      const { data } = await axios.get("http://127.0.0.1:8000/api/barbers/");
+      setBarbers(data);
+    }
+    fetchBarbers();
+  }, []);
   return (
     <div>
       <h2 className="py-3">Barbers</h2>
