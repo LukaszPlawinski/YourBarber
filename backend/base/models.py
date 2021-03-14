4c9fca79fe401b58255f88afb9ec9e3b6f6c6eed
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-14 08:35:14 +0000

    Created super user and did migrations

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Client model
+
+# class Service(models.Model):
+#      user = models.OneToOneField(User, on_delete=models.SET_NULL,null=False)
+#      serviceName = models.CharField(max_length= 200,null = True, blank = True)
+#      price = models.DecimalField(max_digits=7, decimal_places=2,null = True, blank = True)
+
