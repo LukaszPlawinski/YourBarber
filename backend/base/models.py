77de24108033fad13e11aa304e29fb4410d944ef
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-14 08:37:41 +0000

    Created Service model

--- backend/base/models.py
+++ backend/base/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 # Client model
 
-# class Service(models.Model):
-#      user = models.OneToOneField(User, on_delete=models.SET_NULL,null=False)
-#      serviceName = models.CharField(max_length= 200,null = True, blank = True)
-#      price = models.DecimalField(max_digits=7, decimal_places=2,null = True, blank = True)
+class Service(models.Model):
+     user = models.OneToOneField(User, on_delete=models.SET_NULL,null=True)
+     serviceName = models.CharField(max_length= 200,null = True, blank = True)
+     price = models.DecimalField(max_digits=7, decimal_places=2,null = True, blank = True)
 
