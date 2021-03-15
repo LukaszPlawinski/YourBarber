b6c773ba470fc6f3d20d5956b303f9303228b276
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-15 06:32:18 +0000

    Removed Container and Row from import

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
