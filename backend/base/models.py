24f7be684cc27af7ea6e092772527e613762a726
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-15 06:32:30 +0000

    Created Service model

--- backend/base/models.py
+++ backend/base/models.py
@@ -8,4 +8,7 @@ class Service(models.Model):
      user = models.OneToOneField(User, on_delete=models.SET_NULL,null=True)
      serviceName = models.CharField(max_length= 200,null = True, blank = True)
      price = models.DecimalField(max_digits=7, decimal_places=2,null = True, blank = True)
+     
+     def __str__(self):
+          return self.serviceName
 
