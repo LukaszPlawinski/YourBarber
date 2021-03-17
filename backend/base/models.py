d735794fe4a9e647788b3479c2978a3505b8d832
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-17 12:35:35 +0000

    Created basic barber model

--- backend/base/models.py
+++ backend/base/models.py
@@ -8,7 +8,16 @@ class Service(models.Model):
      user = models.OneToOneField(User, on_delete=models.SET_NULL,null=True)
      serviceName = models.CharField(max_length= 200,null = True, blank = True)
      price = models.DecimalField(max_digits=7, decimal_places=2,null = True, blank = True)
+     _id = models.AutoField(primary_key=True,editable=False)
      
      def __str__(self):
           return self.serviceName
 
+class Barber (models.Model):
+     user = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+     # image
+     nickname = models.CharField(max_length=200, null=True,blank=True)
+     description= models.TextField(null=True,blank=True)
+     salary=models.DecimalField(max_digits=7,decimal_places=2,null=True,blank=True)
+     hiredAt= models.DateTimeField()
+     _id = models.AutoField(primary_key=True,editable=False)
