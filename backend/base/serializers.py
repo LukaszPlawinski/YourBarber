79d2292ea063866f7e37733aeab1d752c74ec7c1
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-21 07:08:42 +0000

    Service serializer created

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Barber
+from .models import Barber, Service
 
 
 class BarberSerializer(serializers.ModelSerializer):
      class Meta:
           model = Barber
+          fields = '__all__'
+
+class ServiceSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Service
           fields = '__all__'
