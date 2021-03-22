7ac1ba6b87bcd74a338f5cd95286c61e62f6fc0e
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-22 14:04:05 +0000

    Renamed pages folder to screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Barbers from "./pages/Barbers";
-import Services from "./pages/Services";
-import Home from "./pages/Home";
+import Barbers from "./screens/Barbers";
+import Services from "./screens/Services";
+import Home from "./screens/Home";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
