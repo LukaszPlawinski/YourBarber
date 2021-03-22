a645283e8b812c04b1c506ea5560f6dc9578221c
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-22 04:42:13 +0000

    created service reducer

---
+++ frontend/src/actions/serviceActions.js
@@ -0,0 +1,5 @@
+import {
+  SERVICE_LIST_REQUEST,
+  SERVICE_LIST_SUCCESS,
+  SERVICE_LIST_FAIL,
+} from "../constants/serviceConstants";
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-
-const reducer = combineReducers({});
+import { serviceListReducer } from "./reducers/serviceReducer";
 
 const initialState = {};
+
+const reducer = combineReducers({
+  serviceList: serviceListReducer,
+});
 
 const middleware = [thunk];
 const store = createStore(
