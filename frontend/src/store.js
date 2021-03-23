3a1baa393f8d7c0e745ae7c81a477c4c9584d5d2
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-03-23 20:33:20 +0000

    added barber reducer to the shop

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { serviceListReducer } from "./reducers/serviceReducers";
+import { barberListReducer } from "./reducers/barberReducers";
 
 const initialState = {};
 
 const reducer = combineReducers({
   serviceList: serviceListReducer,
+  barberList: barberListReducer,
 });
 
 const middleware = [thunk];
