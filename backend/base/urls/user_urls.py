a11f2f841ac57eebacbad8d021a3a05146efc2f0
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-02 21:22:51 +0100

    added registerUser path

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -4,7 +4,9 @@ from base.views import user_views as views
 
 urlpatterns = [
      path('', views.getUsers, name="users"),
-     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-     path('profile/', views.getUserProfile, name="users-profile")
+     path('/register', views.registerUser, name="register"),
+     path('/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+     path('/profile/', views.getUserProfile, name="users-profile"),
+     
    
 ]
