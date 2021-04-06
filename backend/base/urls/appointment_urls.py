2d2faf73a5b48e70b7cdbb7cc335814f70b5ef8d
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-06 14:01:58 +0100

    removed slash from path

--- backend/base/urls/appointment_urls.py
+++ backend/base/urls/appointment_urls.py
@@ -4,6 +4,6 @@ from base.views import appointment_views as views
 
 urlpatterns = [
      path('', views.getAppointments, name="appointments"),
-     path('/create', views.createAppointment, name="appointment-create"),
-     path('/<str:pk>', views.getAppointmentById, name="appointment"),
+     path('create', views.createAppointment, name="appointment-create"),
+     path('<str:pk>', views.getAppointmentById, name="appointment"),
 ]
