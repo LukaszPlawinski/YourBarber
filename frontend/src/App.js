78ff68010c04457f2f3364b48b5b15ca9953c97e
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-06 14:03:06 +0100

    added login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Barbers from "./screens/Barbers";
 import Services from "./screens/Services";
 import Home from "./screens/Home";
+import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <main>
         <Container className="text-center py-4">
           <Route path={["/home", "/"]} component={Home} exact />
+          <Route path="/login" component={LoginScreen} />
           <Route path="/services" component={Services} exact />
           <Route path="/barbers" component={Barbers} exact />
         </Container>
