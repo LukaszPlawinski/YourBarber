d7d5f5afdb5f397ea113ec468f9a636da3684285
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-14 22:09:23 +0100

    added set service feature

--- frontend/src/components/Service.js
+++ frontend/src/components/Service.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { ListGroup } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserService } from "../actions/serviceActions";
 
 function Service({ service }) {
+  const dispatch = useDispatch();
+  const serviceList = useSelector((state) => state.serviceList);
+  const handleClick = () => {
+    dispatch(setUserService(service));
+  };
   return (
     <ListGroup.Item>
       {service.serviceName} - {service.price}
