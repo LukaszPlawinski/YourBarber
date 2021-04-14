22d2da655fdba3b6583d3f4bb490d9a864ad6e02
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-14 21:56:08 +0100

    added Booking and Registration screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,10 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Barbers from "./screens/Barbers";
 import Services from "./screens/Services";
+import Booking from "./screens/Booking";
 import Home from "./screens/Home";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -16,8 +18,10 @@ function App() {
         <Container className="text-center py-4">
           <Route path={["/home", "/"]} component={Home} exact />
           <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
           <Route path="/services" component={Services} exact />
           <Route path="/barbers" component={Barbers} exact />
+          <Route path="/booking" component={Booking} />
         </Container>
       </main>
       <Footer />
