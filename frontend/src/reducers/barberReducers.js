b891a92e54602eaa21a0244eda6d155fa655822e
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-14 22:19:36 +0100

    added BARBER_USER_SET

--- frontend/src/reducers/barberReducers.js
+++ frontend/src/reducers/barberReducers.js
@@ -2,16 +2,22 @@ import {
   BARBER_LIST_REQUEST,
   BARBER_LIST_SUCCESS,
   BARBER_LIST_FAIL,
+  BARBER_USER_SET,
 } from "../constants/barberConstants";
 
-export const barberListReducer = (state = { barbers: [] }, action) => {
+export const barberListReducer = (
+  state = { barbers: [], user_barber: {} },
+  action
+) => {
   switch (action.type) {
     case BARBER_LIST_REQUEST:
-      return { loading: true, barbers: [] };
+      return { ...state, loading: true, barbers: [] };
     case BARBER_LIST_SUCCESS:
-      return { loading: false, barbers: action.payload };
+      return { ...state, loading: false, barbers: action.payload };
     case BARBER_LIST_FAIL:
-      return { loading: false, error: action.payload };
+      return { ...state, loading: false, error: action.payload };
+    case BARBER_USER_SET:
+      return { ...state, user_barber: action.user_barber };
     default:
       return state;
   }
