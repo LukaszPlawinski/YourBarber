dc296c50376fc1c2d1fd524807829db8d6db4342
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-17 21:43:21 +0100

    Payment confirmatio

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  const { is_paid } = useSelector((state) => state.appointmentsList);
+
+  const handleClick = () => {
+    dispatch(setUserDate(""));
+    dispatch(setUserService({}));
+    dispatch(setBarber({}));
+    dispatch(setAppointmentIsPaid(false));
+  };
+
+  if (is_paid == true) {
+    return (
+      <div>
+        <p>Succesfully added appointment</p>
+        <Button onClick={handleClick}>Okay</Button>
+      </div>
+    );
+  } else {
+    return <div>Home</div>;
+  }
 }
 
 export default Home;
