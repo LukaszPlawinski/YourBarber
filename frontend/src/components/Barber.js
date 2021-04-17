76ebcfc24921d48e74d4715be8c72e86a50cf41b
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-17 21:38:29 +0100

    highlight on select

--- frontend/src/components/Barber.js
+++ frontend/src/components/Barber.js
@@ -9,17 +9,37 @@ function Barber({ barber }) {
   const handleClick = () => {
     dispatch(setBarber(barber));
   };
-  return (
-    <Card className="p-4 my-3 mx-3">
-      <a href={`/barber/${barber._id}`}>
-        <Card.Img src={barber.image} />
-      </a>
-      <Card.Body>
-        <Card.Title>{barber.nickname}</Card.Title>
-        <Button variant="outline-warning">Book</Button>
-      </Card.Body>
-    </Card>
-  );
+  if (barberList.user_barber._id == barber._id) {
+    return (
+      <Card bg={"primary"} className="p-4 my-3 mx-3">
+        <a href={`/barber/${barber._id}`}>
+          <Card.Img src={barber.image} />
+        </a>
+        <Card.Body>
+          <Card.Title>{barber.nickname}</Card.Title>
+          <Button onClick={handleClick} variant="outline-warning">
+            Select
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  } else {
+    return (
+      <Card //bg={'primary'}
+        className="p-4 my-3 mx-3"
+      >
+        <a href={`/barber/${barber._id}`}>
+          <Card.Img src={barber.image} />
+        </a>
+        <Card.Body>
+          <Card.Title>{barber.nickname}</Card.Title>
+          <Button onClick={handleClick} variant="outline-warning">
+            Select
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default Barber;
--- frontend/src/components/Service.js
+++ frontend/src/components/Service.js
@@ -9,11 +9,23 @@ function Service({ service }) {
   const handleClick = () => {
     dispatch(setUserService(service));
   };
-  return (
-    <ListGroup.Item>
-      {service.serviceName} - {service.price}
-    </ListGroup.Item>
-  );
+  if (serviceList.user_service._id == service._id) {
+    return (
+      <ListGroup.Item
+        style={{ cursor: "pointer" }}
+        active
+        onClick={handleClick}
+      >
+        {service.serviceName} - {service.price}
+      </ListGroup.Item>
+    );
+  } else {
+    return (
+      <ListGroup.Item style={{ cursor: "pointer" }} onClick={handleClick}>
+        {service.serviceName} - {service.price}
+      </ListGroup.Item>
+    );
+  }
 }
 
 export default Service;
