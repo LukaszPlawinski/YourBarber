822847c6a595d39f3ff4755f4be1f844c7fa99d0
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-19 04:56:54 +0100

    created endpoind for myappointments and payment

--- backend/base/urls/appointment_urls.py
+++ backend/base/urls/appointment_urls.py
@@ -5,5 +5,7 @@ from base.views import appointment_views as views
 urlpatterns = [
      path('', views.getAppointments, name="appointments"),
      path('create', views.createAppointment, name="appointment-create"),
+     path('createPaymentIntent', views.createPaymentIntent, name="payment-intent"),
+     path('myappointments', views.getMyAppointments, name="my-appointments"),
      path('<str:pk>', views.getAppointmentById, name="appointment"),
 ]
