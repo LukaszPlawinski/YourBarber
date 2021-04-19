17ece86b24337b610108d6ef755d77b2cba1b345
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-19 05:01:45 +0100

    created redirect to home page after paying

--- frontend/src/components/CheckoutForm.js
+++ frontend/src/components/CheckoutForm.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { Button } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setAppointmentIsPaid } from "../actions/appointmentsActions";
 import axios from "axios";
 
-function CheckoutForm({ amount, barber, service, appointment, history }) {
+import { useHistory } from "react-router-dom";
+
+function CheckoutForm({ amount, barber, service, appointment, user }) {
   const dispatch = useDispatch();
   const stripe = useStripe();
+  let history = useHistory();
+
   const elements = useElements();
   const handleSubmit = async (event) => {
     // Block native form submission.
@@ -48,11 +52,13 @@ function CheckoutForm({ amount, barber, service, appointment, history }) {
         });
 
         console.log(barber, service, appointment.toISOString());
+
         const bodyAppointmentData = new FormData();
         bodyAppointmentData.append("barber", barber._id);
         bodyAppointmentData.append("service", service._id);
         bodyAppointmentData.append("date", appointment.toISOString());
         bodyAppointmentData.append("isPayed", "True");
+        bodyAppointmentData.append("user", user._id);
 
         await axios({
           method: "post",
