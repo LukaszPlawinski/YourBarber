3b939b2601c6392792d839b73bae9b23d2aba419
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-19 05:05:28 +0100

    imported neccessary actions

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,4 +1,13 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setUserDate,
+  setAppointmentIsPaid,
+} from "../actions/appointmentsActions";
+import { setBarber } from "../actions/barberActions";
+import { setUserService } from "../actions/serviceActions";
+import { Button } from "react-bootstrap";
+import Message from "../components/Message";
 
 function Home() {
   const dispatch = useDispatch();
@@ -14,7 +23,7 @@ function Home() {
   if (is_paid == true) {
     return (
       <div>
-        <p>Succesfully added appointment</p>
+        <Message variant="success">Succesfully added appointment</Message>
         <Button onClick={handleClick}>Okay</Button>
       </div>
     );
