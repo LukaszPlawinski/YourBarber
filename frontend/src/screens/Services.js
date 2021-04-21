9b09f6123ad21f212c953605300f5478de5c02a8
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-21 06:59:35 +0100

    created btn to redirect to booking screen

--- frontend/src/screens/Services.js
+++ frontend/src/screens/Services.js
@@ -1,28 +1,33 @@
 import React, { useEffect } from "react";
-import { Row, Col, ListGroup } from "react-bootstrap";
+import { Row, Col, ListGroup, Button } from "react-bootstrap";
 import Service from "../components/Service";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { useDispatch, useSelector } from "react-redux";
 import { listServices } from "../actions/serviceActions";
+import { useHistory } from "react-router-dom";
 
-function Services() {
+function Services({ location }) {
   const dispatch = useDispatch();
   const serviceList = useSelector((state) => state.serviceList);
   const { error, loading, services } = serviceList;
+  let history = useHistory();
+  const handleClick = () => {
+    history.push("/booking");
+  };
   useEffect(() => {
     dispatch(listServices());
   }, [dispatch]);
 
   return (
     <div>
-      <h2 className="py-3">Services</h2>
+      <h4 className="py-3">Select Service</h4>
       {loading ? (
         <Loader />
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Row className="justify-content-md-center">
+        <Row className="justify-content-sm-center">
           <ListGroup variant="flush">
             {services.map((service) => (
               <Col key={service._id} sm={12}>
@@ -32,6 +37,18 @@ function Services() {
           </ListGroup>
         </Row>
       )}
+      {Object.keys(serviceList.user_service).length !== 0 &&
+      location.pathname == "/services" ? (
+        <Button
+          variant="outline-warning"
+          onClick={handleClick}
+          style={{
+            marginTop: "30px",
+          }}
+        >
+          Book appointment
+        </Button>
+      ) : null}
     </div>
   );
 }
