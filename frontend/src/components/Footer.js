7e95f68f9fd8d88acb243724096d8ed80a50a357
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-21 22:21:18 +0100

    added copyright footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
-     return (
-          <footer>
-               <Container className="text-center">
-                    <Row>
-                         <Col>Footer</Col>
-                    </Row>
-              </Container>
-          </footer>
-     )
+  return (
+    <footer>
+      <Container className="text-center">
+        <Row>
+          <Col>
+            <p class="copyright text-muted">
+              Copyright &copy; Lukasz Plawinski 2021
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
