47d9d48bf460753fce1c430f32907fe4b3777e37
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-21 06:55:22 +0100

    created userListReducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -14,6 +14,9 @@ import {
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_RESET,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -70,6 +73,18 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case USER_UPDATE_PROFILE_RESET:
       return {};
+    default:
+      return state;
+  }
+};
+export const userListReducer = (state = { users: [] }, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { ...state, loading: true, users: [] };
+    case USER_LIST_SUCCESS:
+      return { ...state, loading: false, users: action.payload };
+    case USER_LIST_FAIL:
+      return { ...state, loading: false, error: action.payload };
     default:
       return state;
   }
