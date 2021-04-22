626e6526971dedc8281936935a25a5b06f6ff8a6
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-22 21:37:54 +0100

    added Form  Container size in props

--- frontend/src/components/FormContainer.js
+++ frontend/src/components/FormContainer.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
-function FormContainer({ children }) {
+function FormContainer({ children, size }) {
   return (
     <Container>
       <Row className="justify-content-md-center">
-        <Col xs={12} md={6}>
+        <Col xs={12} md={size}>
           {children}
         </Col>
       </Row>
