c34a807c59dd8425019726dbdd8fec9b5b0821d1
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-22 21:39:09 +0100

    Added about navlink to nav

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -25,13 +25,16 @@ function Header() {
     <header>
       <Navbar bg="dark" variant="dark" className="justify-content-center">
         <Nav>
+          <LinkContainer to="/home">
+            <Nav.Link className="mt-2 ml-3 ">About</Nav.Link>
+          </LinkContainer>
           <LinkContainer to="/services">
-            <Nav.Link className="mt-2 ">Services</Nav.Link>
+            <Nav.Link className="mt-2 ml-3 ">Services</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/barbers">
-            <Nav.Link className="mt-2">Barbers</Nav.Link>
+            <Nav.Link className="mt-2 ml-3 ">Barbers</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/">
+          <LinkContainer to="/home">
             <Navbar.Brand className="ml-3 ">
               <img
                 src="/images/Logo.png"
@@ -43,10 +46,10 @@ function Header() {
             </Navbar.Brand>
           </LinkContainer>
           <LinkContainer to="/booking">
-            <Nav.Link className="mt-2">Booking</Nav.Link>
+            <Nav.Link className="mt-2 ">Booking</Nav.Link>
           </LinkContainer>
           {userInfo ? (
-            <NavDropdown className="mt-2" title={userInfo.name} id="username">
+            <NavDropdown className="mt-2 " title={userInfo.name} id="username">
               <LinkContainer to="/profile">
                 <NavDropdown.Item>Profile</NavDropdown.Item>
               </LinkContainer>
@@ -66,9 +69,12 @@ function Header() {
             </NavDropdown>
           ) : (
             <LinkContainer to="/login">
-              <Nav.Link className="mt-2">Login</Nav.Link>
+              <Nav.Link className="mt-2 ">Login</Nav.Link>
             </LinkContainer>
           )}
+          <LinkContainer to="/contact">
+            <Nav.Link className="mt-2 ">Contact</Nav.Link>
+          </LinkContainer>
         </Nav>
       </Navbar>
     </header>
