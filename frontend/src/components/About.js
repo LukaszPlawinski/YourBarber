29ff7fe8da2cb49f3eb3142c652de4e8bc7cc61f
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-22 21:36:21 +0100

    created about component

---
+++ frontend/src/components/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+import FormContainer from "./FormContainer";
+import { Row, Col } from "react-bootstrap";
+
+function About() {
+  return (
+    <div>
+      <FormContainer>
+        <Row>
+          <Col>
+            <img src="/images/about.jpg" alt="" />
+          </Col>
+          <Col>
+            <p>
+              Your Barber Shop was created for real guys who demand more than a
+              10-minute haircut. Our salon is not just a barbershop - it's a
+              place where men can feel special. Thanks to qualified employees
+              working in our salon, professional beard and moustache styling is
+              possible. We are committed to ensuring that our services are at
+              the highest level, and customers using them are satisfied. We make
+              sure that hair cutting is a pleasure for men, therefore you will
+              not have to wait in line with us. We will make an appointment with
+              you for a specific day and time, so you won't lose precious time,
+              and we will have enough of it to take care of your hair.
+            </p>
+          </Col>
+        </Row>
+      </FormContainer>
+    </div>
+  );
+}
+
+export default About;
