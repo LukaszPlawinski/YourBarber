12ded2f83e748011d720a4afa33158c81544c203
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2021-04-22 21:36:54 +0100

    created component with contact details

---
+++ frontend/src/components/ContactDetails.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
+
+function ContactDetails() {
+  return (
+    <div>
+      <Row className="justify-content-sm-center">
+        <Col xs={12} md={4}>
+          <Card className="text-center my-4" style={{ border: "none" }}>
+            <Card.Body>
+              <Card.Title className="mb-4">Address</Card.Title>
+              <Card.Text>
+                <p>Mullingar Rd</p>
+                <p>Dublin</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} md={4}>
+          <Card className="text-center my-4" style={{ border: "none" }}>
+            <Card.Body>
+              <Card.Title className="mb-4">Get in Touch</Card.Title>
+              <Card.Text>
+                <p>Contac@contact.com</p>
+                <p>0894213123</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} md={4}>
+          <Card className="text-center my-4" style={{ border: "none" }}>
+            <Card.Body>
+              <Card.Title className="mb-4">Oppening hours</Card.Title>
+              <Card.Text>
+                <p>Mon-Fri: 8am-5pm</p>
+                <p>Sat-Sun: Closed</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ContactDetails;
